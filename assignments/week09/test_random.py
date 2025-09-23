a5385cbedd543b122dd9ee52f40281bdf2f05c33
Tanawuttanun Homsilp <Tanawuttanun.h@ku.th>
2025-09-23 12:09:56 +0700

    add

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,21 @@
 import random
 
 def test_random():
-    random_number = random.randint(1, 100)
+    # สุมเลขระหว่าง1-10เก็บไว้ในตัวแปรชื่อ  random_number
+    random_number = random.randint(1,10)
+   
+    # รับค่าการทายเลขจากผุ้ใช้ เก็บไว้ในตัวแปรชื่อ guess_number
+    guess_number = input("What is your guess number?: ")
+
+    if random_number == guess_number:
+        print("Congratulations")
+
+    if random_number < guess_number:
+        print("Too much")
+
+    if random_number > guess_number:
+        print("too low")    
+
     print(random_number)
-    
+
 test_random()
